ff61a007c1863cf013668033832cae6ee58c7bd0
Juliana <juliana.fulanetto@aluno.senai.br>
2025-02-26 12:01:26 -0300

    Estrutura React

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import foto from "./img/gatoBlack.jpg"
+import foto from "./img/gatoBlack.jpg";
 
 import Perfil from "./components/perfil/perfil";
 import Switch from "./components/switch/Switch";
@@ -9,45 +9,53 @@ import Rodape from "./components/rodape/Rodape";
 import { useState } from "react";
 
 const App = () => {
-  const [isLight, setIsLight] = useState(true)
+  const [isLight, setIsLight] = useState(true);
 
   const troca = () => {
     setIsLight(!isLight);
   };
   return (
-      <div id="App" className={isLight && "light"}>
-        <Perfil fotoPerfil={foto}> @Jujusac</Perfil>
-        
-        <Switch troca={troca} isLight={isLight}/>
+    <div id="App" className={isLight && "light"}>
+      <Perfil fotoPerfil={foto}> @Jujusac</Perfil>
 
-        <ul>
-          <Links link={"https://github.com/JulianaFulanetto"}>GitHub</Links>
-          <Links link={"https://www.instagram.com/jujuumf_/?hl=pt"}>Instagram</Links>
-          <Links link={"https://br.pinterest.com/julianafulanetto/"}>Pinterest</Links>
-          <Links link={"https://open.spotify.com/user/ax798ppcwgb5tt7kvexntcocm"}>Spotify</Links>
-        </ul>
+      <Switch troca={troca} isLight={isLight} />
 
-        <div id="socialLinks">
-          <SocialLinks
+      <ul>
+        <Links link={"https://github.com/JulianaFulanetto"}>GitHub</Links>
+        <Links link={"https://www.instagram.com/jujuumf_/?hl=pt"}>
+          Instagram
+        </Links>
+        <Links link={"https://br.pinterest.com/julianafulanetto/"}>
+          Pinterest
+        </Links>
+        <Links link={"https://open.spotify.com/user/ax798ppcwgb5tt7kvexntcocm"}>
+          Spotify
+        </Links>
+      </ul>
+
+      <div id="socialLinks">
+        <SocialLinks
           link={"https://github.com/JulianaFulanetto"}
           icon={"logo-github"}
-          />
-          <SocialLinks
+        />
+        <SocialLinks
           link={"https://www.instagram.com/jujuumf_/?hl=pt"}
           icon={"logo-instagram"}
-          />
-          <SocialLinks
+        />
+        <SocialLinks
           link={"https://www.youtube.com/@julianafulanetto4288"}
           icon={"logo-youtube"}
-          />
-          <SocialLinks
-          link={"https://www.linkedin.com/feed/?trk=sem-ga_campid.12619604099_asid.149519181115_crid.725790844702_kw.linkedin_d.c_tid.kwd-148086543_n.g_mt.e_geo.9100701"}
+        />
+        <SocialLinks
+          link={
+            "https://www.linkedin.com/feed/?trk=sem-ga_campid.12619604099_asid.149519181115_crid.725790844702_kw.linkedin_d.c_tid.kwd-148086543_n.g_mt.e_geo.9100701"
+          }
           icon={"logo-linkedin"}
-          />
-        </div>
-
-        <Rodape>Juliana</Rodape>
+        />
       </div>
+
+      <Rodape>Juliana</Rodape>
+    </div>
   );
 };
 
