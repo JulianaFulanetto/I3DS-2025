cd9aba9397fa9fd2ba46149f2ed0232932190da9
Juliana <juliana.fulanetto@aluno.senai.br>
2025-04-22 10:56:45 -0300

    Iniciando Promoção

---
+++ 07-devSteam/src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Promotion = () => {
+  return (
+    <div id="promotion">
+        <h2>Promotion</h2>
+        <div id= "itensPromo">
+    {/* inserindo cards de promoções */}
+        </div>
+    </div>
+  );
+};
+
+export default Promotion
