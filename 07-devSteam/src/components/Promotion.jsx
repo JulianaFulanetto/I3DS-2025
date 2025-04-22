0332ce292f94561858181ef64d425bd5b70e78cb
Juliana <juliana.fulanetto@aluno.senai.br>
2025-04-22 14:18:33 -0300

    Estruturando o PromoCard

--- 07-devSteam/src/components/Promotion.jsx
+++ 07-devSteam/src/components/Promotion.jsx
@@ -5,12 +5,14 @@ const Promotion = () => {
   return (
     <div id="promotion">
         <h2>Promotion</h2>
-        <div id= "itensPromo">
+        <div id= "itensPromo" className='d-flex flex-wrap gap-4 justify-content-around'>
     {/* inserindo cards de promoções */}
+            <PromoCard />
+            <PromoCard />
             <PromoCard />
         </div>
     </div>
   );
 };
 
-export default Promotion
+export default Promotion;
